d530b0ff0ac3d4ab91a326e17099c380a446a27e
Peter Banh <peterbanhe.e@gmail.com>
2023-03-04 16:03:51 -0600

    Basic Movement and Collision.
    + Completed walking/idle animations. + Changed from Discrete Collision -
    + Detection to Continuous on Gloob (fixed issue where Gloob would randomly stop moving while walking after starting his idle animation).
    + Added new sprites to spritesheet1
    + Adjusted custom physics Collision.

--- Gloob/Assets/Scripts/PlayerInput.cs
+++ Gloob/Assets/Scripts/PlayerInput.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public gloob gloob;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        gloob.Move(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Time.fixedDeltaTime);
     }
 }
--- Gloob/Assets/Scripts/gloob.cs
+++ Gloob/Assets/Scripts/gloob.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class gloob: MonoBehaviour{
 
-    private void Start(){
-
+    Rigidbody2D rd;
+    SpriteRenderer spriteR;
+    public AnimtionScript anm;
+    public float speed = 2.5f;
+    void Start(){
+        rd = GetComponent<Rigidbody2D>();
+        spriteR = GetComponent<SpriteRenderer>();
     }
-    private void Update(){
+    public void Move(Vector3 offset){
+        if(offset !=Vector3.zero){
+            Debug.Log(offset);
+            rd.MovePosition(transform.position + ((offset) * speed));
+            anm.ChangeAnimationState("Walking");
 
-    }
-    private void FixedUpdate(){
-        
+            if(offset.x < 0){
+                spriteR.flipX = true;
+            }
+            else{
+                spriteR.flipX = false;
+            }
+        }
+        else{
+            anm.ChangeAnimationState("Idle");
+        }
     }
 }
