22e672a54a6b3f4a3b501ac1f17d4cf1474d6b74
Peter Banh <peterbanhe.e@gmail.com>
2023-03-10 16:10:51 -0600

    Currency system
    + Added basic gem pick up

--- Gloob/Assets/Scripts/PlayerInput.cs
+++ Gloob/Assets/Scripts/PlayerInput.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public gloob gloob;
+    public Rigidbody2D rd;
 
-    public void FixedUpdate(){
-        gloob.Move();
-        gloob.gloobJump();
+    //Handles Player Input
+    void Update(){
+        float maxGravity;
+        float velocityY = rd.velocity.y;
+        bool ground = gloob.isGround();
+        
+        gloob.Horizontal = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Jump") && ground){
+            gloob.Jump = true;
+            Debug.Log("Jump Running");
+        }
+        else if(!ground && (Input.GetButtonUp("Jump") ||velocityY < 0.0f)){
+            gloob.JumpApex = true;
+        }
+        if(Input.GetButtonUp("Horizontal")){
+            gloob.stop();
+        }
+        if(velocityY < (maxGravity = gloob.MaxGravity)){
+            gloob.capGravity();
+        }
     }
+
   
 }
--- Gloob/Assets/Scripts/gloob.cs
+++ Gloob/Assets/Scripts/gloob.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 
 public class gloob: MonoBehaviour{
 
-    [SerializeField] Rigidbody2D rd;
+    [SerializeField]  Rigidbody2D rd;
     [SerializeField] SpriteRenderer spriteR;
     [SerializeField] BoxCollider2D box2D;
     [SerializeField] LayerMask platformMask;
@@ -13,44 +13,26 @@ public class gloob: MonoBehaviour{
 
     [Header ("Movement")]
     [SerializeField] float speed = 2.5f;
-    public bool jump = false, jumpApex = false;
+    [SerializeField] bool jump = false, jumpApex = false;
     [SerializeField] float jumpHeight = 5f, fallSpeed = 200f;
-    [SerializeField] float Friction=0.25f, maxGravity;
+    [SerializeField] float maxGravity;
     [SerializeField] float horizontal;
    
    [Header ("Events")]
     public UnityEvent gemPickUp;
    
-
     void Start(){
         rd = GetComponent<Rigidbody2D>();
         box2D = GetComponent<BoxCollider2D>();
         spriteR = GetComponent<SpriteRenderer>();
     }
 
-
-    void Update(){
-        horizontal = Input.GetAxisRaw("Horizontal");
-        bool ground = isGround();
-        if(Input.GetButtonDown("Jump") && ground){
-            
-            jump = true;
-        }
-        else if(!ground && (Input.GetButtonUp("Jump") || rd.velocity.y < 0.0f)){
-            jumpApex = true;
-        }
-        //implements very simplistic friction
-        if(Input.GetButtonUp("Horizontal")){
-            rd.velocity = (new Vector2 (rd.velocity.x * Friction, rd.velocity.y));
-        }
-        if(rd.velocity.y < maxGravity){
-            rd.velocity=(new Vector2(rd.velocity.x, maxGravity));
-        }
+    private void FixedUpdate() {
+        Move();
+        gloobJump();   
     }
-    
-
+    //Moves Gloob and changes animation
     public void Move(){
-
         if(horizontal !=0){
             rd.velocity = new Vector2(horizontal * speed, ((rd.velocity.y)));
             anm.ChangeAnimationState("Walking", 1);
@@ -67,6 +49,7 @@ public class gloob: MonoBehaviour{
         }
         
     }
+    // Adds jumping mechanic using ground check
     public void gloobJump(){
         if(jump){
             jump=false;
@@ -81,13 +64,70 @@ public class gloob: MonoBehaviour{
             rd.AddForce(Vector2.down * fallSpeed, ForceMode2D.Impulse);
         }
     }
-
-    private bool isGround(){
+    //========== Helper functions ==========
+    // stops gloob's vertical movement.
+    public void stop(){
+         rd.velocity = (new Vector2 (0, rd.velocity.y));
+    }
+    // enforces max fall speed on gloob.
+    public void capGravity(){
+        rd.velocity=(new Vector2(rd.velocity.x, maxGravity));
+    }
+    // ground check
+    public bool isGround(){
         return Physics2D.BoxCast(box2D.bounds.center, box2D.bounds.size, 0f, Vector2.down, 0.1f, platformMask);
     }
-        void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("platform")){
-            rd.velocity = (new Vector2(rd.velocity.x, 0));
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("gem")){
+            test();
+        }
+    }
+    public void test(){
+        Debug.Log("Testing");
+        gemPickUp?.Invoke();
+    }
+    // ========== Properties ==========
+    public Rigidbody2D RD{
+        get{
+            return rd;
+        }
+        set{
+            rd = value;
+        }
+    }
+    public bool Jump{
+        get{
+            return jump;
+        }
+        set{
+            jump = value;
+        }
+    }
+    public bool JumpApex{
+        get{
+            return jumpApex;
+        }
+        set{
+            jumpApex = value;
+        }
+    }
+    public float MaxGravity{
+        get{
+            return maxGravity;
+        }
+        set{
+            maxGravity = value;
+        }
+    }
+
+    public float Horizontal{
+        get{
+            return horizontal;
+        }
+        set{
+            horizontal = value;
         }
     }
 }
+
